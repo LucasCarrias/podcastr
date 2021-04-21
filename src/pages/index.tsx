3dca5d9337dde4a34babf412d7c76f36018bebbc
Lucas Carrias <lucascarrias@outlook.com>
2021-04-21 11:18:51 -0300

    List lastest episodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { GetStaticProps } from 'next';
+import Image from 'next/image';
 import { format, parseISO} from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+import styles from './home.module.scss';
 
 type Episode = {
   id: string,
@@ -17,14 +19,48 @@ type Episode = {
 }
 
 type HomeProps = {
-  episodes: Episode[]
+  latestEpsiodes: Episode[],
+  allEpisodes: Episode[]
 }
 
-export default function Home(props: HomeProps) {
+export default function Home({ latestEpsiodes, allEpisodes }: HomeProps) {
   return (
-  <div>
-    <h1>Index</h1>
-    <p>{JSON.stringify(props.episodes)}</p>
+  <div className={styles.homepage}>
+    <section className={styles.latestEpisodes}>
+      <h2>Últimos lançamentos</h2>
+
+      <ul>
+        {latestEpsiodes.map(episode => {
+          return (
+            <li key={episode.id}>
+              <Image
+                width={192}
+                height={192}
+                src={episode.thumbnail}
+                alt={episode.title}
+                objectFit="cover"
+              />
+
+              <div className={styles.episodeDetails}>
+                <a href="">{episode.title}</a>
+                <p>{episode.members}</p>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.durationAsString}</span>
+              </div>
+
+              <button type="button">
+                <img src="/play-green.svg" alt="Tocar episódio"/>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+
+    </section>
+
+    <section className={styles.allEpisodes}>
+
+    </section>
   </div>
   )
 }
@@ -51,11 +87,15 @@ export const  getStaticProps: GetStaticProps = async () => {
       description: episode.description,
       url: episode.file.url
     };
-  })
+  });
+
+  const latestEpsiodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.lengt)
   
   return {
     props: {
-      episodes,
+      latestEpsiodes,
+      allEpisodes
     },
     revalidate: 60 * 60 * 8,
   }
